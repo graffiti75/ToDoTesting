54498a96f04aeab499890bf6f7b407fb79c5e55a
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-03-22 14:57:23 -0400

    Adding code from section "5.3.9. (3:20) Write Your First Test Continued"

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -47,4 +47,32 @@ class StatisticsUtilsTest {
 		assertEquals(result.completedTasksPercent, 40f)
 		assertEquals(result.activeTasksPercent, 60f)
 	}
+
+	@Test
+	fun getActiveAndCompletedStats_empty_returnsZeros() {
+
+		// Create an active tasks (the false makes this active)
+		val tasks = emptyList<Task>()
+
+		// Call our function
+		val result = getActiveAndCompletedStats(tasks)
+
+		// Check the result
+		assertEquals(result.completedTasksPercent, 0f)
+		assertEquals(result.activeTasksPercent, 0f)
+	}
+
+	@Test
+	fun getActiveAndCompletedStats_error_returnsZeros() {
+
+		// Create an active tasks (the false makes this active)
+		val tasks = null
+
+		// Call our function
+		val result = getActiveAndCompletedStats(tasks)
+
+		// Check the result
+		assertEquals(result.completedTasksPercent, 0f)
+		assertEquals(result.activeTasksPercent, 0f)
+	}
 }
