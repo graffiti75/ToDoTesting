baaa4a70be35345928ea7e8549eba40193809ac8
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-03-25 22:57:07 -0400

    Adding code from section "5.3.17. (0:00) Write Your Own ViewModel Test"

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -3,8 +3,8 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.CoreMatchers.not
-import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Rule
 import org.junit.Test
@@ -30,5 +30,20 @@ class TasksViewModelTest {
 		assertThat(
 			value.getContentIfNotHandled(), not(nullValue())
 		)
+	}
+
+	// - Sets the filtering mode to ALL_TASKS
+	// - Assets that the tasksAddViewVisible LiveData is true
+	@Test
+	fun setFilterAllTasks_tasksAddViewVisible() {
+		// Given a fresh ViewModel
+		val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+		// When the filter type is ALL_TASKS
+		tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+		// Then the "Add task" action is visible
+		val value = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
+		assertThat(value, `is`(true))
 	}
 }
