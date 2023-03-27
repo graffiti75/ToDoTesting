a40b60bb04eae89bd5a569826237d7ec8494b994
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-03-27 14:28:36 -0400

    Adding code from section "5.3.18. (0:00) Solution: Write Your Own ViewModel Test"

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -3,9 +3,9 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,15 +13,20 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
+	// Subject under test
+	private lateinit var tasksViewModel: TasksViewModel
+
 	// Executes each task synchronously using Architecture Components.
 	@get:Rule
 	var instantExecutorRule = InstantTaskExecutorRule()
 
+	@Before
+	fun setupViewModel() {
+		tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+	}
+
 	@Test
 	fun addNewTask_setsNewTaskEvent() {
-		// Given a fresh ViewModel
-		val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-
 		// When adding a new task
 		tasksViewModel.addNewTask()
 
@@ -36,9 +41,6 @@ class TasksViewModelTest {
 	// - Assets that the tasksAddViewVisible LiveData is true
 	@Test
 	fun setFilterAllTasks_tasksAddViewVisible() {
-		// Given a fresh ViewModel
-		val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-
 		// When the filter type is ALL_TASKS
 		tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
 
