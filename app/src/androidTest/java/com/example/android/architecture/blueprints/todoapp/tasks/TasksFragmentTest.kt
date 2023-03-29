40daabbed5edc63cacf26fbf44af30f0f9f74801
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-03-29 16:18:58 -0400

    Adding code from section "5.4.17. (5:44) Using Mockito to Write Navigation Tests" - Implementing test clickAddTaskButton_navigateToAddEditFragment()

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
@@ -1,16 +1,19 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.R
 import com.example.android.architecture.blueprints.todoapp.ServiceLocator
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.FakeAndroidTestRepository
@@ -20,7 +23,6 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import com.example.android.architecture.blueprints.todoapp.R
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
@@ -58,14 +60,46 @@ class TasksFragmentTest {
 		onView(withId(R.id.tasks_list))
 			.perform(
 				RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
-					hasDescendant(withText("TITLE1")
-				), click())
+					hasDescendant(
+						withText("TITLE1")
+					), click()
+				)
 			)
 
 		// THEN - Verify that we navigate to the first detail screen
 		verify(navController)
 			.navigate(
-				TasksFragmentDirections.actionTasksFragmentToTaskDetailFragment( "id1")
+				TasksFragmentDirections.actionTasksFragmentToTaskDetailFragment("id1")
+			)
+	}
+
+	/**
+	 * Write the test clickAddTaskButton_navigateToAddEditFragment which checks that
+	 * if you click on the + FAB, you navigate to the AddEditTaskFragment.
+	 */
+	@Test
+	fun clickAddTaskButton_navigateToAddEditFragment() = runTest {
+		// GIVEN - On the tasks screen with two tasks
+//		repository.saveTask(Task("TITLE1", "DESCRIPTION1", false, "id1"))
+//		repository.saveTask(Task("TITLE2", "DESCRIPTION2", true, "id2"))
+
+		// GIVEN - On the home screen
+		val scenario = launchFragmentInContainer<TasksFragment>(Bundle(), R.style.AppTheme)
+		val navController = mock(NavController::class.java)
+		scenario.onFragment {
+			Navigation.setViewNavController(it.view!!, navController)
+		}
+
+		// WHEN  - Click on the + FAB
+		onView(withId(R.id.add_task_fab)).perform(click())
+
+		// THEN - Verify that we navigate to the AddEditTaskFragment
+		verify(navController)
+			.navigate(
+				TasksFragmentDirections.actionTasksFragmentToAddEditTaskFragment(
+					null,
+					getApplicationContext<Context>().getString(R.string.add_task)
+				)
 			)
 	}
 }
