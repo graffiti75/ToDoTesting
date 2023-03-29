03106b56812081e10b69038727605d5c9f367035
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-03-28 23:29:38 -0400

    Adding code from section "5.4.12. (6:13) Use Your Service Locator in your Tests"

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -1,6 +1,7 @@
 package com.example.android.architecture.blueprints.todoapp
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import androidx.room.Room
 import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource
@@ -8,12 +9,20 @@ import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepo
 import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource
 import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
 import com.example.android.architecture.blueprints.todoapp.data.source.remote.TasksRemoteDataSource
+import kotlinx.coroutines.runBlocking
 
+/**
+ * A Service Locator for the [TasksRepository]. This is the prod version, with
+ * the "real" [TasksRemoteDataSource].
+ */
 object ServiceLocator {
+
     private var database: ToDoDatabase? = null
+    private val lock = Any()
 
     @Volatile
     var tasksRepository: TasksRepository? = null
+        @VisibleForTesting set
 
     fun provideTasksRepository(context: Context): TasksRepository {
         synchronized(this) {
@@ -39,5 +48,21 @@ object ServiceLocator {
         ).build()
         database = result
         return result
+    }
+
+    @VisibleForTesting
+    fun resetRepository() {
+        synchronized(lock) {
+            runBlocking {
+                TasksRemoteDataSource.deleteAllTasks()
+            }
+        }
+        // Clear all data to avoid test pollution.
+        database?.apply {
+            clearAllTables()
+            close()
+        }
+        database = null
+        tasksRepository = null
     }
 }
